7ea573b45ff5424d0e03ec1cb20f31f856a550c2
emrea <emreakburak94@gmail.com>
2024-07-30 17:22:44 +0300

    JWT expiration set to 1 minute

--- src/main/java/com/emre/security/util/JwtUtil.java
+++ src/main/java/com/emre/security/util/JwtUtil.java
@@ -29,7 +29,7 @@ public class JwtUtil {
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
-
+//
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
