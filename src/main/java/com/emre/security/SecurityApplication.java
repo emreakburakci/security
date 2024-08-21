37dc0ba4aebd00dd6607c541f29d1fd204afcb98
emrea <emreakburak94@gmail.com>
2024-08-21 11:07:40 +0300

    DEV branch test

--- src/main/java/com/emre/security/SecurityApplication.java
+++ src/main/java/com/emre/security/SecurityApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+
+// DEV TEST
 @SpringBootApplication
 @EnableScheduling
 public class SecurityApplication extends SpringBootServletInitializer {
